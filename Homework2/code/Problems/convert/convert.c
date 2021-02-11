71780c07aeb87168fd85bc04f8e1c678d3bfe42f
microuzi9797 <ken97971@yahoo.com.tw>
2021-02-12 01:51:44 +0800

    Instructions

--- Homework2/code/Problems/convert/convert.c
+++ Homework2/code/Problems/convert/convert.c
@@ -6,14 +6,14 @@ int convert(char *);
 
 int main()
 {
-    char input[SIZE];
-    int out;
+	char input[SIZE];
+	int out;
 
-    while (scanf("%s", input) != EOF) {
-        out = convert(input);
+	while (scanf("%s", input) != EOF) {
+		out = convert(input);
 	printf("%d\n", out);
-    }
+	}
 
-    return 0;
+	return 0;
 }
 
